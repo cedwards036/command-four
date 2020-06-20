4c740236e88f899f9d0d89f58bfdd22f324cc0d5
Christopher Edwards <cedwards036@gmail.com>
2020-06-20 16:10:58 -0400

    check to see if the game is over after each move

--- lib/board.rb
+++ lib/board.rb
@@ -1,42 +1,212 @@
 class Board
-    def initialize(width, height)
-        @width = width
-        @height = height
-        @board = Array.new(@width) {Array.new(@height, :empty)}
+  def initialize(width = 7, height = 6, connect_n = 4)
+    @width = width
+    @height = height
+    @connect_n = connect_n
+    @state = GameState.new(false)
+    @board = Array.new(@width) {Array.new(@height, :empty)}
+    @completed_moves = 0
+    @max_moves = width * height
+  end
+
+  def drop_piece(column, color)
+    if game_over?()
+      raise PieceDropError.new("Game is already over")
+    elsif invalid_index?(column)
+      raise PieceDropError.new("Invalid column index: #{column}")
+    elsif full?(column)
+      raise PieceDropError.new("Column #{column} is already full")
+    else
+      first_empty_cell_index = @board[column].index {|cell| cell == :empty}
+      @board[column][first_empty_cell_index] = color
+      @completed_moves += 1
+      @state = ConnectNChecker.new(@board, @connect_n, column, first_empty_cell_index).check
+      if @completed_moves >= @max_moves && !game_over?()
+        @state = GameState.new(true)
+      end
+    end
+  end
+
+  def game_over?
+    @state.game_over?
+  end
+
+  def self.from_a(arr, connect_n = 4)
+    board = Board.new(arr.length, arr[0].length, connect_n)
+    for i in 0...arr[0].length
+      for j in 0...arr.length
+        if arr[j][i] != :empty
+          board.drop_piece(j, arr[j][i])
+        end
+      end
+    end
+    board
+  end
+
+  def to_a
+    @board
+  end
+
+  class PieceDropError < StandardError
+  end
+
+  private
+
+  def invalid_index?(column)
+    column < 0 || column >= @width
+  end
+
+  def full?(column)
+    @board[column][-1] != :empty
+  end
+
+  class ConnectNChecker
+
+    def initialize(board, connect_n, col_idx, row_idx)
+      @col_idx = col_idx
+      @row_idx = row_idx
+      @cur_color = board[col_idx][row_idx]
+      @width = board.length
+      @height = board[0].length
+      @board = board
+      @connect_n = connect_n
     end
 
-    def drop_piece(column, color)
-        if column < 0 || column >= @width
-            raise PieceDropError.new("Invalid column index: #{column}")
-        elsif full?(column)
-            raise PieceDropError.new("Column #{column} is already full")
+    def check
+      if @cur_color != :empty
+        game_is_over = [
+          check_horizontal,
+          check_vertical,
+          check_left_diag,
+          check_right_diag
+        ].reduce(false) {|result, check| result || check}
+        if game_is_over
+          GameState.new(true)
         else
-            first_empty_cell_index = @board[column].index {|cell| cell == :empty}
-            @board[column][first_empty_cell_index] = color
+          GameState.new(false)
         end
+      else 
+        GameState.new(false)
+      end
     end
 
-    def self.from_a(arr)
-        board = Board.new(arr.length, arr[0].length)
-        for i in 0...arr[0].length
-            for j in 0...arr.length
-                board.drop_piece(j, arr[j][i])
-            end
-        end
-        board
+    def check_horizontal
+      total_count = get_right_count + get_left_count - 1
+      total_count >= @connect_n
     end
 
-    def to_a
-        @board
+    def check_vertical
+      total_count = get_up_count + get_down_count - 1
+      total_count >= @connect_n
     end
 
-    class PieceDropError < StandardError
+    def check_left_diag
+      total_count = get_up_left_count + get_down_left_count - 1
+      total_count >= @connect_n
     end
 
-    private
-
-    def full?(column)
-        @board[column][-1] != :empty
+    def check_right_diag
+      total_count = get_up_right_count + get_down_right_count - 1
+      total_count >= @connect_n
     end
+
+    def get_right_count
+      consecutive_count = 0
+      cur_col_idx = @col_idx
+      while cur_col_idx < @width && consecutive_count <= @connect_n && @board[cur_col_idx][@row_idx] == @cur_color
+        cur_col_idx += 1
+        consecutive_count += 1
+      end
+      consecutive_count
+    end
+
+    def get_left_count
+      consecutive_count = 0
+      cur_col_idx = @col_idx
+      while cur_col_idx >= 0 && consecutive_count <= @connect_n && @board[cur_col_idx][@row_idx] == @cur_color
+        cur_col_idx -= 1
+        consecutive_count += 1
+      end
+      consecutive_count
+    end
+
+    def get_up_count
+      consecutive_count = 0
+      cur_row_idx = @row_idx
+      while cur_row_idx < @height && consecutive_count <= @connect_n && @board[@col_idx][cur_row_idx] == @cur_color
+        cur_row_idx += 1
+        consecutive_count += 1
+      end
+      consecutive_count
+    end
+
+    def get_down_count
+      consecutive_count = 0
+      cur_row_idx = @row_idx
+      while cur_row_idx >= 0 && consecutive_count <= @connect_n && @board[@col_idx][cur_row_idx] == @cur_color
+        cur_row_idx -= 1
+        consecutive_count += 1
+      end
+      consecutive_count
+    end
+
+    def get_up_left_count
+      consecutive_count = 0
+      cur_row_idx = @row_idx
+      cur_col_idx = @col_idx
+      while cur_row_idx < @height && cur_col_idx >= 0 && consecutive_count <= @connect_n && @board[cur_col_idx][cur_row_idx] == @cur_color
+        cur_row_idx += 1
+        cur_col_idx -= 1
+        consecutive_count += 1
+      end
+      consecutive_count
+    end
+
+    def get_down_left_count
+      consecutive_count = 0
+      cur_row_idx = @row_idx
+      cur_col_idx = @col_idx
+      while cur_row_idx >= 0 && cur_col_idx < @width && consecutive_count <= @connect_n && @board[cur_col_idx][cur_row_idx] == @cur_color
+        cur_row_idx -= 1
+        cur_col_idx += 1
+        consecutive_count += 1
+      end
+      consecutive_count
+    end
+
+    def get_up_right_count
+      consecutive_count = 0
+      cur_row_idx = @row_idx
+      cur_col_idx = @col_idx
+      while cur_row_idx < @height && cur_col_idx < @width && consecutive_count <= @connect_n && @board[cur_col_idx][cur_row_idx] == @cur_color
+        cur_row_idx += 1
+        cur_col_idx += 1
+        consecutive_count += 1
+      end
+      consecutive_count
+    end
+
+    def get_down_right_count
+      consecutive_count = 0
+      cur_row_idx = @row_idx
+      cur_col_idx = @col_idx
+      while cur_row_idx >= 0 && cur_col_idx >= 0 && consecutive_count <= @connect_n && @board[cur_col_idx][cur_row_idx] == @cur_color
+        cur_row_idx -= 1
+        cur_col_idx -= 1
+        consecutive_count += 1
+      end
+      consecutive_count
+    end
+  end
+
+  class GameState
+    def initialize(game_is_over)
+      @game_is_over = game_is_over
+    end
+  
+    def game_over?
+      @game_is_over
+    end
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,46 +2,103 @@ require "./lib/board"
 
 describe Board do
 
-    describe "#drop_piece" do 
-        it "adds the piece to the bottom of an empty column" do
-            board = Board.new(2, 2)
-            board.drop_piece(0, :red)
-            expect(board.to_a).to eq([[:red, :empty], [:empty, :empty]])
-        end
-
-        it "adds the piece to the lowest empty cell in a non-empty column" do
-            board = Board.new(2, 2)
-            board.drop_piece(1, :red)
-            board.drop_piece(1, :yellow)
-            expect(board.to_a).to eq([[:empty, :empty], [:red, :yellow]])
-        end
-
-        it "throws an exception when attempting to drop in an invalid column" do
-            board = Board.new(2, 2)
-            expect {board.drop_piece(3, :red)}.to raise_error(Board::PieceDropError)
-            expect {board.drop_piece(-1, :red)}.to raise_error(Board::PieceDropError)
-        end
-
-        it "throws an exception when attempting to drop in a full column" do
-            board = Board.new(2, 2)
-            board.drop_piece(1, :red)
-            board.drop_piece(1, :red)
-            expect {board.drop_piece(1, :red)}.to raise_error(Board::PieceDropError)
-        end
+  describe "#drop_piece" do 
+    it "adds the piece to the bottom of an empty column" do
+      board = Board.new(2, 2)
+      board.drop_piece(0, :red)
+      expect(board.to_a).to eq([[:red, :empty], [:empty, :empty]])
     end
 
-    describe "#to_a" do
-        it "returns a 2D array representing the current state of the board" do
-            board = Board.new(2, 2)
-            expect(board.to_a).to eq([[:empty, :empty], [:empty, :empty]])
-        end
+    it "adds the piece to the lowest empty cell in a non-empty column" do
+      board = Board.new(2, 2)
+      board.drop_piece(1, :red)
+      board.drop_piece(1, :yellow)
+      expect(board.to_a).to eq([[:empty, :empty], [:red, :yellow]])
     end
 
-    describe "#from_a" do
-        it "creates a board matching the state of the given 2D array" do
-            original_array = [[:red, :red], [:yellow, :empty]]
-            board = Board.from_a(original_array)
-            expect(board.to_a).to eq(original_array)
-        end
+    it "throws an exception when attempting to drop in an invalid column" do
+      board = Board.new(2, 2)
+      expect {board.drop_piece(3, :red)}.to raise_error(Board::PieceDropError)
+      expect {board.drop_piece(-1, :red)}.to raise_error(Board::PieceDropError)
     end
+
+    it "throws an exception when attempting to drop in a full column" do
+      board = Board.new(2, 2)
+      board.drop_piece(1, :red)
+      board.drop_piece(1, :red)
+      expect {board.drop_piece(1, :red)}.to raise_error(Board::PieceDropError)
+    end
+  end
+
+  describe "#check_board" do
+    it "the game is still in progress if no connect-ns have occurred" do
+      board = Board.new(2, 2)
+      expect(board.game_over?).to be false
+    end
+
+    it "the game ends if there is a horizontal connect-n" do
+      board = Board.from_a([
+        [:red, :empty, :empty, :empty],
+        [:red, :empty, :empty, :empty],
+        [:red, :empty, :empty, :empty],
+        [:empty, :empty, :empty, :empty],
+      ], 3)
+      expect(board.game_over?).to be true
+    end
+
+    it "the game ends if there is a vertical connect-n" do
+      board = Board.from_a([
+        [:red, :empty, :empty, :empty],
+        [:red, :yellow, :yellow, :yellow],
+        [:empty, :empty, :empty, :empty],
+        [:empty, :empty, :empty, :empty],
+      ], 3)
+      expect(board.game_over?).to be true
+    end
+
+    it "the game ends if there is a left-diagonal connect-n" do
+      board = Board.from_a([
+        [:red, :empty, :empty, :empty],
+        [:red, :yellow, :red, :yellow],
+        [:yellow, :red, :yellow, :empty],
+        [:yellow, :yellow, :empty, :empty],
+      ], 3)
+      expect(board.game_over?).to be true
+    end
+
+    it "the game ends if there is a right-diagonal connect-n" do
+      board = Board.from_a([
+        [:red, :empty, :empty, :empty],
+        [:red, :yellow, :yellow, :empty],
+        [:yellow, :red, :red, :empty],
+        [:yellow, :yellow, :red, :empty],
+      ], 3)
+      expect(board.game_over?).to be true
+    end
+
+    it "the game ends if the board is full with no connect-ns" do
+      board = Board.from_a([
+        [:red, :red, :yellow, :yellow],
+        [:red, :yellow, :yellow, :yellow],
+        [:red, :red, :red, :yellow],
+        [:yellow, :yellow, :red, :red],
+      ], 4)
+      expect(board.game_over?).to be true
+    end
+  end
+
+  describe "#to_a" do
+    it "returns a 2D array representing the current state of the board" do
+      board = Board.new(2, 2)
+      expect(board.to_a).to eq([[:empty, :empty], [:empty, :empty]])
+    end
+  end
+
+  describe "#from_a" do
+    it "creates a board matching the state of the given 2D array" do
+      original_array = [[:red, :red], [:yellow, :empty]]
+      board = Board.from_a(original_array)
+      expect(board.to_a).to eq(original_array)
+    end
+  end
 end
